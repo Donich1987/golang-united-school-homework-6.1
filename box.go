028f904c8349a42a52f845d5ccebaf78d24082e0
Donich1987 <99492348+Donich1987@users.noreply.github.com>
2022-06-01 23:11:38 +0300

    Update box.go

--- box.go
+++ box.go
@@ -33,7 +33,6 @@ func (b *box) AddShape(shape Shape) error {
 // GetByIndex allows getting shape by index
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
-	chislo := len(b.shapes)
 	if  i < 0 {
 		return nil, errors.New("errors, GetByIndex")
 	}
