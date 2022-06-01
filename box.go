905932b0d6b8f1d95bc340acf3f2c1fd4356c0bb
Donich1987 <99492348+Donich1987@users.noreply.github.com>
2022-06-01 23:50:35 +0300

    Update box.go

--- box.go
+++ box.go
@@ -34,11 +34,11 @@ func (b *box) AddShape(shape Shape) error {
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
 	chislo := len(b.shapes)
-	if   chislo <= i ||i < 0 {
-		return nil, errors.New("errors, GetByIndex")
+		if i >= chislo || i < 0 {
+		return nil, errors.New("the element does not exist")
+	} else {
+		return b.shapes[i], nil
 	}
-	
-	return b.shapes[i], nil
 }
 
 // ExtractByIndex allows getting shape by index and removes this shape from the list.
